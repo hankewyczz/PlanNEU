043ebfc6155914b76c3c735d43b322d31cae6590
hankewyczz <hankewyczz@gmail.com>
2022-04-14 10:08:29 -0400

    WIP

--- frontend/pages/contexts/FiltersContext.tsx
+++ frontend/pages/contexts/FiltersContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, ReactElement, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { useRouter } from "next/router";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const emptyFilters = {
     courses: [],
@@ -18,16 +20,22 @@ const FiltersContext = createContext(emptyFilters);
 export const FiltersProvider = ({ children }): ReactElement => {
     const router = useRouter();
     const [formData, setFormData] = useState(emptyFilters);
+    const [hasValidQuery, setHasValidQuery] = useState(true);
 
     useEffect(() => {
         if (router.isReady) {
-            const raw_courses = router.query["courses"];
-            const arr_courses =
-                typeof raw_courses === "string" ? raw_courses.split(",") : raw_courses;
-            const courses = arr_courses.map((c) => c.toUpperCase());
+            const rawCourses = router.query["courses"];
+            const rawTermId = router.query["term-id"];
+            
+            if (!rawCourses || !rawTermId) {
+                setHasValidQuery(false);
+                return;
+            }
 
-            const raw_termid = router.query["term-id"];
-            const termid = typeof raw_termid === "string" ? raw_termid : raw_termid[0];
+            const arrCourses =
+                typeof rawCourses === "string" ? rawCourses.split(",") : rawCourses;
+            const courses = arrCourses.map((c) => c.toUpperCase());
+            const termid = typeof rawTermId === "string" ? rawTermId : rawTermId[0];
 
             setFormData({
                 ...formData,
@@ -39,7 +47,24 @@ export const FiltersProvider = ({ children }): ReactElement => {
     }, [router.isReady, router.query]);
 
     const { Provider } = FiltersContext;
-    return <Provider value={formData}>{children}</Provider>;
+    return (
+        hasValidQuery 
+        ? <Provider value={formData}>{children}</Provider> 
+        : <Popup defaultOpen={true} closeOnDocumentClick={false} closeOnEscape={false}>
+            <div className="modalContainer">
+                <h2 className="header">Schedule Setup</h2>
+                <p>Please enter the courses you'd like to schedule, and the term/semester.</p>
+                <p><b>Courses</b> should be formatted with the subject code followed by the course ID number.</p>
+                <ul>
+                    <li>✅ CS2500</li>
+                    <li>✅ CS 2500</li>
+                    <li>✅ cs 2500</li>
+                    <li>❌ CS-2500</li>
+                    <li>❌ fundies</li>
+                    <li>❌ Fundamentals of Computer Science 1</li>
+                </ul>
+            </div>
+        </Popup>);
 };
 
 FiltersProvider.propTypes = {
