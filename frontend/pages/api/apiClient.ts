008191540dcc73669b28d077577f4dda141d25b9
hankewyczz <hankewyczz@gmail.com>
2022-04-14 20:32:30 -0400

    Update env varaible insertion vercel

--- frontend/pages/api/apiClient.ts
+++ frontend/pages/api/apiClient.ts
@@ -1,5 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 import {  getSdk } from "../../generated/graphql";
 
+const ENDPOINT = process.env.BACKEND_API;
 
-export const gqlClient = getSdk(new GraphQLClient(process.env.BACKEND_API));
+export const gqlClient = getSdk(new GraphQLClient(ENDPOINT));
