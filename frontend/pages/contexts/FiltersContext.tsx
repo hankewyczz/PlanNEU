a3dc624384b990b752811edd8e84474deadb1dde
hankewyczz <hankewyczz@gmail.com>
2022-04-15 11:35:33 -0400

    Fix input field default

--- frontend/pages/contexts/FiltersContext.tsx
+++ frontend/pages/contexts/FiltersContext.tsx
@@ -119,7 +119,7 @@ export const FiltersProvider = ({ children }): ReactElement => {
                                 {({ input, meta }) => (
                                     <div>
                                         <label>Term ID:</label><span className='gap'></span>
-                                        <input {...input} type="text" value="202310" placeholder="Term ID" />
+                                        <input {...input} type="text" placeholder="Term ID (use 202310 for Fall 2022)" />
                                         <span className='gap'></span>
                                         {meta.error && meta.touched && <span>{meta.error}</span>}
                                     </div>
