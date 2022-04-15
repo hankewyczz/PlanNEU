8839c642785da55205a6948cafb727d0c190cc70
hankewyczz <hankewyczz@gmail.com>
2022-04-14 20:16:36 -0400

    Fix backend

--- frontend/pages/api/apiClient.ts
+++ frontend/pages/api/apiClient.ts
@@ -2,4 +2,4 @@ import { GraphQLClient } from "graphql-request";
 import {  getSdk } from "../../generated/graphql";
 
 
-export const gqlClient = getSdk(new GraphQLClient("http://localhost:4001"));
+export const gqlClient = getSdk(new GraphQLClient(process.env.BACKEND_API));
