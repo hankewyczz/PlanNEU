775d33f96c26ce2f3f8877a741a9eaac1575a71b
hankewyczz <hankewyczz@gmail.com>
2022-04-14 20:38:59 -0400

    use next_public variables

--- frontend/pages/api/apiClient.ts
+++ frontend/pages/api/apiClient.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 import {  getSdk } from "../../generated/graphql";
 
-const ENDPOINT = process.env.BACKEND_API;
+const ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
 
 export const gqlClient = getSdk(new GraphQLClient(ENDPOINT));
